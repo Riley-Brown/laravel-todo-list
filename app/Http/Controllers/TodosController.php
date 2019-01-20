5d451c94359466bbc0837dfcca6a239fac16a5c8
Riley-Brown <riley@riley.gg>
2019-01-20 05:18:54 -0500

    Added views to display todos on index route

--- app/Http/Controllers/TodosController.php
+++ app/Http/Controllers/TodosController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Todo;
+
 
 class TodosController extends Controller
 {
@@ -13,7 +15,8 @@ class TodosController extends Controller
      */
     public function index()
     {
-        //
+        $todos = Todo::all();
+        return view('todos.index')->with('todos', $todos);
     }
 
     /**
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Todo List</title>
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+
+<body>
+    <div class="container">
+
+        @yield('content')
+
+    </div>
+
+    <footer id="footer">
+        <p class="text-center">Copyright &copy; 2019 Todo List </p>
+    </footer>
+
+</body>
+
+</html>
