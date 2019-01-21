4461323d7c217152ae09814e707fcb8e130a6588
Riley-Brown <riley@riley.gg>
2019-01-21 16:50:01 -0500

    Added delete button form component to handle deleting todo

--- resources/views/todos/show.blade.php
+++ resources/views/todos/show.blade.php
@@ -2,20 +2,25 @@
 
 @section('content')
 
-<a href="/" class="btn btn-secondary mb-3">Go Back</a>
-<h1>
-    <a href="todo/{{$todo->id}}">
-        {{$todo->text}}
+    <a href="/" class="btn btn-secondary mb-3">Go Back</a>
+    <h1>
+        <a href="todo/{{$todo->id}}">
+            {{$todo->text}}
 
-    </a>
-</h1>
-<div class="badge badge-danger p-2">{{$todo->due}}</div>
-<hr>
-<p>{{$todo->body}}</p>
+        </a>
+    </h1>
+    <div class="badge badge-danger p-2">{{$todo->due}}</div>
+    <hr>
+    <p>{{$todo->body}}</p>
 
-<br>
-<br>
+    <br>
+    <br>
 
-<a href="/todo/{{$todo->id}}/edit" class="btn btn-light">Edit</a>
+    <a href="/todo/{{$todo->id}}/edit" class="btn btn-light">Edit</a>
+
+    {!! Form::open(['action' => ['TodosController@destroy', $todo->id], 'method' => 'POST', 'class' => 'float-right']) !!}
+        {{ Form::hidden('_method', 'DELETE') }}
+        {{ Form::bsSubmit('Delete', ['class' => 'btn btn-danger']) }}
+    {!! Form::close() !!}
 
 @endsection
